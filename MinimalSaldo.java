65da57a6553daafb86b327322518bbcf910c1b8c
bladeclara42 <71927457+bladeclara42@users.noreply.github.com>
2021-12-05 22:42:34 +0700

    Cute Words
    Perubahan variabel minSaldoku ke saldoAwal

---
+++ MinimalSaldo.java
@@ -0,0 +1,8 @@
+class MinimalSaldo extends Saldo {
+	
+double saldoAwal;
+public double minSaldo(){
+	saldoAwal= 50000;
+	return saldoAwal;
+}
+}
--- Saldo2.java
+++
@@ -1,8 +0,0 @@
-class Saldo2 extends Saldo {
-	
-double minSaldoku;
-public double minSaldo(){
-	minSaldoku= 50000;
-	return minSaldoku;
-}
-}
